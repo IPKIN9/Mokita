7ccd3899b625d4e5c0248b26cb2ceb1899396f9b
IPKIN9 <irwandirini@gmail.com>
2022-11-14 13:44:02 +0800

    Create pagination & limit

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -35,7 +35,7 @@ class Authenticate
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if ($this->auth->guard($guard)->guest()) {
+        if ($this->auth->guard($guard)->guest() && config('app.env') != 'local') {
             return response('Unauthorized.', 401);
         }
 
--- app/Ucase/Interfaces/HakimInterface.php
+++ app/Ucase/Interfaces/HakimInterface.php
@@ -2,8 +2,9 @@
 
 namespace App\Ucase\Interfaces;
 
-interface HakimInterface {
-  public function getAllData();
+interface HakimInterface
+{
+  public function getAllData($limit, $page);
   public function getDataById($id);
   public function upsertData($id, array $detail);
   public function deleteData($id);
